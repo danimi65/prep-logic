be753f0d63a97d5f5d0d46c1cce00b9c770de7f3
Danika Harada <danikaharada@Danikas-MacBook-Air.local>
2016-09-12 01:21:19 -1000

    completed totalUnderWhat

--- functions.js
+++ functions.js
@@ -141,10 +141,17 @@ function totalOver30 (first, second, third){
     return false;
 }
 console.log(totalOver30(50, 60, 70));
+
 /*Function - totalUnderWhat
 Write a function that takes four variables of type Number called first and second and third and fourth respectively. Return true if the sum of first, second and third are less than fourth otherwise return false.*/
 
-
+function totalUnderWhat(first, second, third, fourth){
+  if (first + second + third < fourth){
+    return true;
+  }
+    return false;
+}
+console.log(totalUnderWhat(10, 18, 20, 120));
 
 /*Function - looptoNumber
 Write a function that takes in a Number value named limit and have the function write a for loop that loops the number of times of limit and console.log each number as the loop executes.*/
